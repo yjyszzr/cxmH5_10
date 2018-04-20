e19fd2f4d99be8e0b25682624c445df43d4a5f5c
jiangyanri <jiangyanrui@haodai.com>
2018-04-20 17:47:18 +0800

    添加收藏页面

--- cxm/src/components/user/collection/index.vue
+++ cxm/src/components/user/collection/index.vue
@@ -3,7 +3,16 @@
 </style>
 <template>
     <div class="wrap collection">
-
+        <ul>
+            <li>
+                <div class="choice">
+                    <h5>意甲争四进入百色花，罗马双雄被看好</h5>
+                    <p>意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花意甲争四进入百色花</p>
+                    <span>彩小秘精选</span><span>今日04-17</span><span>阅读9898989</span>
+                </div>
+            </li>
+            <li></li>
+        </ul>
     </div>
 </template>
 <script src='./script.js'>
