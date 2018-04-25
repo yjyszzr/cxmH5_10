d0aff56cd97ed5115b40942fdabbfa2ae211394d
jiangyanri <jiangyanrui@haodai.com>
2018-04-25 16:08:32 +0800

    帮助，服务

---
+++ cxm/src/components/user/help/script.js
@@ -0,0 +1,15 @@
+import api from '../../../fetch/api'
+export default {
+    name: 'help',
+    data () {
+      return {
+        
+      }
+    },
+    created(){
+      
+    },
+    mounted(){
+        
+    }
+}
