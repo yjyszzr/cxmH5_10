12145ecb587eda0ea13da9d2104518dbea44fde9
yanrui <18603579728@163.com>
2018-05-29 15:17:19 +0800

    添加世界杯活动

--- cxm/src/components/activity/world_cup/index.vue
+++ cxm/src/components/activity/world_cup/index.vue
@@ -7,11 +7,17 @@
         <div class="img">
             <img src="./image/img.jpg" alt="玩转世界杯，激情一夏">
         </div>
-        <div class="cup_list">
+        <div class="information">
             <ul>
                 <li>
-                    <v-informal :zxList='zxList'></v-informal>
+                    <v-informal :zxList='findList'></v-informal>
                 </li>
+                <div class="triple" v-show="trFlag">
+                    <mt-spinner type="triple-bounce" color="#999"></mt-spinner>&nbsp;<span>正在加载</span>
+                </div>
+                <div class="cxLoad" v-show="cxLoadFlag" @click="cxLoadClick()">
+                    加载失败,点击重试
+                </div>
             </ul>
         </div>
     </div>
--- cxm/src/components/activity/world_cup/script.js
+++ cxm/src/components/activity/world_cup/script.js
@@ -1,55 +1,86 @@
 import {isTitle} from '../../../util/common'
 import api from '../../../fetch/api'
-import { Toast } from 'mint-ui'
-import QRCode from "qrcode";
+import {Indicator, Toast} from 'mint-ui'
+import informal from '../../public/informal/informalList'
+
 export default {
     name: 'world_cup',
-    data(){
+    data() {
         return {
             token: '',
-            zxList:[]
+            page: 1,
+            findList: [],
+            trFlag: true,
+            cxLoadFlag: false,
+            isbool: true,
+            findObj: {},
         }
     },
     beforeCreate() {
-        Indicator.open();
+        Indicator.open()
     },
     components: {
         "v-informal": informal
     },
-    mounted(){
-        isTitle('世界杯')
+    mounted() {
+        document
+            .querySelector("#content")
+            .addEventListener("scroll", this.handleScroll);
+        this.fetchData()
+        isTitle("世界杯")
     },
     methods:{
-
-    },
-    fetchData() {
-        let data = {
-            page: this.page,
-            size: 20
-        };
-        api
-            .dllist(data)
-            .then(res => {
-                if (res.code == 0) {
-                    //console.log(res);
-                    this.zxObj = res.data;
-                    if (this.page == 1) {
-                        this.zxList = this.zxList.concat(res.data.list);
-                        this.trFlag = false
-                    } else {
-                        setTimeout(() => {
-                            this.trFlag = false;
-                            this.isbool = true;
-                            this.zxList = this.zxList.concat(res.data.list);
-                        }, 800);
+        fetchData(){
+            let data = {
+                extendCat: '4',
+                page: this.page,
+                size: 3
+            }
+            api.findList(data)
+                .then(res => {
+                    if (res.code == 0) {
+                        this.findObj = res.data
+                        if (this.page == 1) {
+                            this.findList = this.findList.concat(res.data.dlArticlePage.list);
+                            this.trFlag = false
+                        } else {
+                            setTimeout(() => {
+                                this.trFlag = false;
+                                this.isbool = true;
+                                this.findList = this.findList.concat(res.data.dlArticlePage.list);
+                            }, 800);
+                        }
                     }
+                })
+                .catch(err=>{
+                    setTimeout(() => {
+                        this.trFlag = false;
+                        this.cxLoadFlag = true;
+                    }, 800);
+                })
+
+        },
+        cxLoadClick(){
+            this.trFlag = true;
+            this.cxLoadFlag = false
+            this.fetchData()
+        },
+        handleScroll(e) {
+            if (
+                document.querySelector("#content").scrollHeight -
+                document.querySelector("#content").clientHeight -
+                document.querySelector("#content").scrollTop <=
+                0 &&
+                this.isbool == true
+            ) {
+                if (this.findObj.dlArticlePage.isLastPage == "false") {
+
+                    this.page++;
+                    this.fetchData();
+                    this.trFlag = true;
+                    this.isbool = false;
                 }
-            })
-            .catch(err=>{
-                setTimeout(() => {
-                    this.trFlag = false;
-                    this.cxLoadFlag = true;
-                }, 800);
-            })
-    },
+            }
+        },
+    }
 }
