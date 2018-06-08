7cdde101cdc173ef8baf97c21ada7c3c28e5d7e7
yanrui <18603579728@163.com>
2018-06-08 16:29:29 +0800

    app

---
+++ cxm/src/components/app_mjb/help/script.js
@@ -0,0 +1,19 @@
+import api from '../../../fetch/api'
+import {means} from '../../../util/common'
+export default {
+    name: 'help',
+    data () {
+      return {
+        
+      }
+    },
+    created(){
+      
+    },
+    methods:{
+     
+    },
+    mounted(){
+      means('帮助中心').isTitle
+    }
+}
