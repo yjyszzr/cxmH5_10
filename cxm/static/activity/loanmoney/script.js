3ea055bd24f3820d367b16ca9362b7ce44692b56
lvjian <18310396880@163.com>
2018-06-20 11:55:24 +0800

    app下载

--- cxm/static/activity/loanmoney/script.js
+++ cxm/static/activity/loanmoney/script.js
@@ -1,4 +1,9 @@
 $.showIndicator();
+		$(window).resize(function() {
+			if(document.activeElement.tagName === 'INPUT'){
+				document.activeElement.scrollIntoView({behavior: "smooth"})
+			}
+		});
 		var baseUrl = 'http://39.106.18.39:8765/api/'
 		// var baseUrl = 'http://api.caixiaomi.net/api/'
 		function getUrlStr(name,url){
