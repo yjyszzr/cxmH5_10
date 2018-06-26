b3eecf38112e6df719da818e016715ffc45d306b
yanrui <18603579728@163.com>
2018-06-26 18:23:12 +0800

    昵称，身份认证

--- cxm/src/components/user/setup/script.js
+++ cxm/src/components/user/setup/script.js
@@ -1,6 +1,7 @@
 import api from '../../../fetch/api'
 import { Toast } from 'mint-ui'
 import { Indicator } from 'mint-ui'
+import { MessageBox } from 'mint-ui'
 export default {
     name: 'record',
     data () {
@@ -12,6 +13,18 @@ export default {
       
     },
     methods:{
+        nickname(){
+            MessageBox.prompt('',{
+                title: '给自己取一个喜欢的昵称',
+                message: '昵称取好后不能再修改',
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                showCancelButton: true
+            }).then(({ value, action }) => {
+            },action => {
+
+            })
+        }
     },
     computed: {
         userInfo(){
