c47f8b5cfa55a8ea081c3d61b6aa3a08f0fbafb6
yanrui <18603579728@163.com>
2018-06-27 17:50:31 +0800

    立即下载

--- cxm/src/components/activity/spread/script.js
+++ cxm/src/components/activity/spread/script.js
@@ -12,26 +12,32 @@ import {
 	MessageBox
 } from 'mint-ui';
 export default {
-	name: 'one',
-	data() {
-		return {
+    name: 'one',
+    data() {
+        return {
             fr: this.$route.query.fr,
-            detect:''
-		}
-	},
-	mounted() {
-	},
-	methods: {
-		lqBtn() {
-            if(detect() === 'ios'){
-                location.href="http://m.caixiaomi.net"
-            }else{
-                if(this.fr){
-                    location.href="http://m.caixiaomi.net/down/cxm106_j"+this.$route.query.fr+".apk"
-                }else {
-                    location.href="https://m.caixiaomi.net/down/cxm106_jc035.apk"
+            detect: ''
+        }
+    },
+    mounted() {
+    },
+    methods: {
+        lqBtn() {
+            if (detect() === 'ios') {
+                location.href = "http://m.caixiaomi.net"
+            } else {
+                if(this.$route.query.fr == 'c042'){
+                        location.href="http://m.caixiaomi.net/down/cxm106_jc042.apk"
+                    }else if(this.$route.query.fr == 'c043'){
+                        location.href="http://m.caixiaomi.net/down/cxm106_jc043.apk"
+                    }else if(this.$route.query.fr == 'c044'){
+                        location.href="http://m.caixiaomi.net/down/cxm106_jc044.apk"
+                    }else if(this.$route.query.fr == 'c045'){
+                        location.href="http://m.caixiaomi.net/down/cxm106_jc045.apk"
+                    }else {
+                    location.href = "https://m.caixiaomi.net/down/cxm106_jc035.apk"
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 }
