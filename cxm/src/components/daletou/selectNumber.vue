884a1dd034b0e856c54df604263a38d5673ab3b3
zhanggangchao <zhanggc34@126.com>
2018-08-01 10:22:22 +0800

    张广超

--- cxm/src/components/daletou/selectNumber.vue
+++ cxm/src/components/daletou/selectNumber.vue
@@ -4,7 +4,24 @@
 
 <script>
     export default {
-        name: "select-number"
+        name: "select-number",
+        data(){
+          return{
+
+          }
+        },
+        created(){
+
+        },
+        beforeRouteLeave(to, from, next) {
+            next()
+            if(to.path!='/lottery/teamDetail'){
+                this.$store.dispatch("getMarkShow",false)
+                this.$store.dispatch("getMarkShowType",'')
+                this.$store.dispatch("getLeagueIds",'')
+                this.$store.dispatch("getMarkDateVal",'')
+            }
+        }
     }
 </script>
 
