6d01693479f38484ed68f5b4f8f84a22b40d9021
zhanggangchao <zhanggc34@126.com>
2019-02-19 10:37:26 +0800

    玩法帮助

--- cxm/src/components/entrySpecies/daletou/playHelp.vue
+++ cxm/src/components/entrySpecies/daletou/playHelp.vue
@@ -3,13 +3,12 @@
         <!--躯干-->
         <div class="body">
             <div class="body-in">
-                <p style="text-align:center;font-size:0.38rem;font-weight:700;">中国体彩大乐透玩法规则</p>
                 <p class="body-in-title">投注截止时间</p>
                 <p class="p">每周一、三、六19：30截止投注</p>
                 <p class="body-in-title">开奖时间</p>
                 <p class="p">每周一、三、六20：30开奖</p>
                 <p class="body-in-title">玩法规则</p>
-                <p class="p">5个红球+2个蓝球=1注（2元），追加投注1元（最多可中1600万）追加投注仅参与一至五等奖的奖金分配，一至三等奖追加投注奖金为当期基本投注对应单注奖金的60%，四至五等奖追加投注奖金为当期基本投注对应单注奖金的50%。</p>
+                <p class="p">5个红号+2个蓝号=1注（2元），追加投注1元（最高可中1800万）追加投注仅参与一，二等奖的奖金分配，同时追加奖金为当期基本投注对应单注奖金的80%，三至九等奖为固定奖金，无追加奖金。</p>
                 <table class="table">
                     <tr>
                         <th>奖级</th>
@@ -48,6 +47,7 @@
                         <td>中5红+1蓝</td>
                         <td>浮动</td>
                     </tr>
+
                     <tr>
                         <td>三等奖</td>
                         <td>
@@ -59,21 +59,12 @@
                                     <span class="redBall"></span>
                                     <span class="redBall"></span>
                                 </div>
-                                <div>
-                                    <span class="redBall"></span>
-                                    <span class="redBall"></span>
-                                    <span class="redBall"></span>
-                                    <span class="redBall"></span>
-                                    <span class="blueBall"></span>
-                                    <span class="blueBall"></span>
-                                </div>
                             </div>
                         </td>
                         <td>
                             <p>中5红</p>
-                            <p>中4红+2蓝</p>
                         </td>
-                        <td>浮动</td>
+                        <td>10000元</td>
                     </tr>
                     <tr>
                         <td>四等奖</td>
@@ -85,21 +76,14 @@
                                     <span class="redBall"></span>
                                     <span class="redBall"></span>
                                     <span class="blueBall"></span>
-                                </div>
-                                <div>
-                                    <span class="redBall"></span>
-                                    <span class="redBall"></span>
-                                    <span class="redBall"></span>
-                                    <span class="blueBall"></span>
                                     <span class="blueBall"></span>
                                 </div>
                             </div>
                         </td>
                         <td>
-                            <p>中4红+1蓝</p>
-                            <p>中3红+2蓝</p>
+                            <p>中4红+2蓝</p>
                         </td>
-                        <td>200元</td>
+                        <td>3000元</td>
                     </tr>
                     <tr>
                         <td>五等奖</td>
@@ -110,7 +94,55 @@
                                     <span class="redBall"></span>
                                     <span class="redBall"></span>
                                     <span class="redBall"></span>
+                                    <span class="blueBall"></span>
                                 </div>
+                            </div>
+
+                        </td>
+                        <td>
+                            <p>中4红+1蓝</p>
+                        </td>
+                        <td>300元</td>
+                    </tr>
+                    <tr>
+                        <td>六等奖</td>
+                        <td>
+                            <div class="td-box">
+                                <div>
+                                    <span class="redBall"></span>
+                                    <span class="redBall"></span>
+                                    <span class="redBall"></span>
+                                    <span class="blueBall"></span>
+                                    <span class="blueBall"></span>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p>中3红+2蓝</p>
+                        </td>
+                        <td>200元</td>
+                    </tr>
+                    <tr>
+                        <td>七等奖</td>
+                        <td>
+                            <div class="td-box">
+                                <div>
+                                    <span class="redBall"></span>
+                                    <span class="redBall"></span>
+                                    <span class="redBall"></span>
+                                    <span class="redBall"></span>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p>中4红</p>
+                        </td>
+                        <td>100元</td>
+                    </tr>
+                    <tr>
+                        <td>八等奖</td>
+                        <td>
+                            <div class="td-box">
                                 <div>
                                     <span class="redBall"></span>
                                     <span class="redBall"></span>
@@ -124,17 +156,15 @@
                                     <span class="blueBall"></span>
                                 </div>
                             </div>
-
                         </td>
                         <td>
-                            <p>中4红</p>
                             <p>中3红+1蓝</p>
                             <p>中2红+2蓝</p>
                         </td>
-                        <td>10元</td>
+                        <td>15元</td>
                     </tr>
                     <tr>
-                        <td>六等奖</td>
+                        <td>九等奖</td>
                         <td>
                             <div class="td-box">
                                 <div>
@@ -144,12 +174,12 @@
                                 </div>
                                 <div>
                                     <span class="redBall"></span>
-                                    <span class="blueBall"></span>
+                                    <span class="redBall"></span>
                                     <span class="blueBall"></span>
                                 </div>
                                 <div>
                                     <span class="redBall"></span>
-                                    <span class="redBall"></span>
+                                    <span class="blueBall"></span>
                                     <span class="blueBall"></span>
                                 </div>
                                 <div>
@@ -160,8 +190,8 @@
                         </td>
                         <td>
                             <p>中3红</p>
-                            <p>中1红+2蓝</p>
                             <p>中2红+1蓝</p>
+                            <p>中1红+2蓝</p>
                             <p>中2蓝</p>
                         </td>
                         <td>5元</td>
